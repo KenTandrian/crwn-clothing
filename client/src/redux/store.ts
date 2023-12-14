5c815def3edba6779ee5ff00ceb61d8ac78cff5b
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2023-12-14 16:47:02 +0700

    fix(ts): ignore Middleware type incompatibility

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -9,6 +9,7 @@ import rootSaga from "./root-saga";
 import rootReducer from "./root-reducer";
 
 const sagaMiddleware = createSagaMiddleware();
+// @ts-ignore
 const middlewares: Middleware[] = [sagaMiddleware]; // we can still add another middlewares here
 
 if (process.env.NODE_ENV === 'development'){
